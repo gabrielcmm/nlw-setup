84e7a89a86342135e0bb6573570d5968a5032cfc
Gabriel Custódio <gabrielcust@gmail.com>
2023-01-20 19:29:54 -0300

    Initial Commit

---
+++ script.js
@@ -0,0 +1,26 @@
+const form = document.querySelector("#form-habits")
+const nlwSetup = new NLWSetup(form)
+const addButton = document.querySelector("header button")
+addButton.addEventListener("click", addEvent)
+form.addEventListener("change", saveEvent)
+
+
+function addEvent() {
+  const today = new Date().toLocaleDateString("pt-br").slice(0, 5)
+  const dayExists = nlwSetup.dayExists(today)
+
+  if (dayExists) {
+    alert("O dia já está incluso no sistema!")
+    return
+  }
+  nlwSetup.addDay(today)
+  alert("Dia Adicionado com sucesso!")
+}
+
+function saveEvent() {
+  localStorage.setItem("NlwSetup@habits", JSON.stringify(nlwSetup.data))
+}
+
+const data = JSON.parse(localStorage.getItem("NlwSetup@habits")) || {}
+nlwSetup.setData(data)
+nlwSetup.load()
